89ab035d3c445ada60baa2db31a7dcfdefea06a4
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2025-10-15 09:56:03 +0700

    Fix test

--- PersonalWeb.Admin/jest.config.ts
+++ PersonalWeb.Admin/jest.config.ts
@@ -1,19 +1,25 @@
 import type { Config } from "jest";
+
 const config: Config = {
   preset: "ts-jest",
   collectCoverage: true,
   testEnvironment: "jsdom",
   collectCoverageFrom: ["**/src/**/*.{ts,tsx}"],
   coverageDirectory: "./coverage/",
-  transformIgnorePatterns: ["<rootDir>/node_modules/"],
+  transformIgnorePatterns: [],
   moduleNameMapper: {
-    "^axios$": require.resolve("axios"),
+    "^axios$": "axios/dist/node/axios.cjs",
     "\\.(css|less|scss|sass)$": "identity-obj-proxy"
+  },
+  transform: {
+    '^.+\\.(ts|tsx)?$': 'ts-jest',
+    '^.+\\.(js|jsx)$': 'babel-jest'
   },
   testMatch: [
     "<rootDir>/src/**/__tests__/**/*.{js,jsx,ts,tsx}",
     "<rootDir>/src/**/*.{spec,test}.{js,jsx,ts,tsx}"
   ],
+  setupFiles: ['<rootDir>/setup.jest.js'],
 };
 
 export default config;
