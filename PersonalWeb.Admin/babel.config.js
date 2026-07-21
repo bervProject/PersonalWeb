8e12ba70309ddca2b56f9d7bb55d202974a0c7de
Bervianto Leo Pratama <bervianto.leo@gmail.com>
2026-07-21 12:02:59 +0700

    Fix Babel invalid browser target error by explicitly specifying valid targets

--- PersonalWeb.Admin/babel.config.js
+++ PersonalWeb.Admin/babel.config.js
@@ -1,3 +1,9 @@
 export default {
-    presets: ['@babel/preset-env']
+    presets: [
+        ['@babel/preset-env', {
+            targets: {
+                browsers: ['>0.2%', 'last 2 versions', 'not dead', 'not ie <= 11']
+            }
+        }]
+    ]
 }
